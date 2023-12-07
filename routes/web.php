a4b1cdcb3ea342ffd387b9da591adbc1097e19b7
deandraaditya <deandra.213040026@mail.unpas.ac.id>
2023-12-08 00:17:55 +0700

    menambahkan tampilan pada halaman register

--- routes/web.php
+++ routes/web.php
@@ -32,3 +32,7 @@ Route::get('/dashboard', function () {
 Route::get('/login', function () {
     return view ('login.index');
 });
+
+Route::get('/register', function () {
+    return view ('register.index');
+});
