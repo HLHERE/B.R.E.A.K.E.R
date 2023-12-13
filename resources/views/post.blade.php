7bb28daec4f1f5c0cb8cc65291b1232268e3f7dd
deandraaditya <deandra.213040026@mail.unpas.ac.id>
2023-12-13 09:08:06 +0700

    Menambahkan halaman pada post

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -2,7 +2,63 @@
 
 @section('container')
 
+<!-- component -->
+<div class="max-w-screen-lg mx-auto">
+<main class="mt-40">
+<!-- component -->
+<div class="max-w-screen-lg mx-auto">
+<main class="-mt-24">
+    <main class="mt-10">
 
+      <div class="mb-4 md:mb-0 w-full mx-auto relative">
+        <div class="px-4 lg:px-0">
+        </div>
 
+        <img src="/../img/valor.jpg" class="w-full object-cover lg:rounded" style="height: 28em;"/>
+      </div>
+
+      <div class="flex flex-col lg:flex-row lg:space-x-12">
+
+        <div class="px-4 lg:px-0 mt-12 text-gray-700 text-lg leading-relaxed w-full lg:w-3/4">
+          <p class="pb-6">Advantage old had otherwise sincerity dependent additions. It in adapted natural hastily is
+            justice. Six draw
+            you him full not mean evil. Prepare garrets it expense windows shewing do an. She projection advantages
+            resolution son indulgence. Part sure on no long life am at ever. In songs above he as drawn to. Gay was
+            outlived peculiar rendered led six.</p>
+
+          <p class="pb-6">Difficulty on insensible reasonable in. From as went he they. Preference themselves me as
+            thoroughly
+            partiality considered on in estimating. Middletons acceptance discovered projecting so is so or. In or
+            attachment inquietude remarkably comparison at an. Is surrounded prosperous stimulated am me discretion
+            expression. But truth being state can she china widow. Occasional preference fat remarkably now projecting
+            uncommonly dissimilar. Sentiments projection particular companions interested do at my delightful. Listening
+            newspaper in advantage frankness to concluded unwilling.</p>
+
+          <p class="pb-6">Adieus except say barton put feebly favour him. Entreaties unpleasant sufficient few pianoforte
+            discovered
+            uncommonly ask. Morning cousins amongst in mr weather do neither. Warmth object matter course active law
+            spring six. Pursuit showing tedious unknown winding see had man add. And park eyes too more him. Simple excuse
+            active had son wholly coming number add. Though all excuse ladies rather regard assure yet. If feelings so
+            prospect no as raptures quitting.</p>
+
+          <div class="border-l-4 border-gray-500 pl-4 mb-6 italic rounded">
+            Sportsman do offending supported extremity breakfast by listening. Decisively advantages nor
+            expression
+            unpleasing she led met. Estate was tended ten boy nearer seemed. As so seeing latter he should thirty whence.
+            Steepest speaking up attended it as. Made neat an on be gave show snug tore.
+          </div>
+
+          <p class="pb-6">Exquisite cordially mr happiness of neglected distrusts. Boisterous impossible unaffected he me
+            everything.
+            Is fine loud deal an rent open give. Find upon and sent spot song son eyes. Do endeavor he differed carriage
+            is learning my graceful. Feel plan know is he like on pure. See burst found sir met think hopes are marry
+            among. Delightful remarkably new assistance saw literature mrs favourable.</p>
+
+        </div>
+      </div>
+    </main>
+    <!-- main ends here -->
+  </div>
+      </div>
 
 @endsection
