6a20791b7889abc3f1b66231035b9efe6a5e14bd
HL <kalfearislit@gmail.com>
2023-12-16 15:21:03 +0700

    merevisi table dashboard

--- resources/views/dashboard/posts/index.blade.php
+++ resources/views/dashboard/posts/index.blade.php
@@ -7,46 +7,44 @@
 
 
 
-<div class="relative top-[125px] w-[1000px]  overflow-x-auto shadow-md sm:rounded-lg"> <!-- Tambahkan class 'mx-auto' di sini -->
+<div class="relative top-[125px] w-[900px]  overflow-x-auto shadow-md sm:rounded-lg table-auto"> <!-- Tambahkan class 'mx-auto' di sini -->
     <table class="relative w-full overflow-x-auto shadow-md sm:rounded-lg ">
         <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
             <tr>
-                <th scope="col" class="px-6 py-3 text-left">
-                    Product Name
+                <th scope="col" class="px-6 py-3 text-left w-12">
+                    #
                 </th>
-                <th scope="col" class="px-6 py-3 text-left">
-                    Color
+                <th scope="col" class="px-6 py-3 text-left w-48">
+                    title
                 </th>
-                <th scope="col" class="px-6 py-3 text-left">
+                <th scope="col" class="px-6 py-3 text-left w-32">
                     Category
                 </th>
-                <th scope="col" class="px-6 py-3 text-left">
-                    Price
-                </th>
-                <th scope="col" class="px-6 py-3 text-left">
+                <th scope="col" class="px-6 py-3 text-left w-32">
                     Action
                 </th>
+                
             </tr>
         </thead>
+        
         <tbody>
             <tr class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
                 <td class="px-6 py-4 whitespace-nowrap font-medium text-gray-900 dark:text-white">
-                    Apple MacBook Pro 17"
+                    1
                 </td>
                 <td class="px-6 py-4">
-                    Silver
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, alias ipsa similique iure pariatur qui veritatis accusamus commodi officia eligendi vel. Delectus cum corrupti laudantium, ipsum ullam nisi. Dicta, nostrum.
                 </td>
                 <td class="px-6 py-4">
                     Laptop
+                </td>   
+                <td class="px-6 py-4 flex items-center ">
+                    <a href="#" class="font-medium text-yellow-400 dark:text-blue-500 hover:underline w-8">Edit</a>
+                    <span class="mx-1">|</span>
+                    <a href="#" class="font-medium text-red-500 dark:text-red-500 hover:underline w-8">Delete</a>
                 </td>
-                <td class="px-6 py-4">
-                    $2999
-                </td>
-                <td class="px-6 py-4 flex items-center">
-                    <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
-                    <span class="mx-2">|</span>
-                    <a href="#" class="font-medium text-red-500 dark:text-red-500 hover:underline">Hapus</a>
-                </td>
+                
+                
             </tr>
             <!-- Add more rows as needed -->
         </tbody>
--- resources/views/dashboard/user/index.blade.php
+++ resources/views/dashboard/user/index.blade.php
@@ -7,50 +7,54 @@
 
 
 
-<div class="relative top-[125px] w-[1000px]  overflow-x-auto shadow-md sm:rounded-lg"> <!-- Tambahkan class 'mx-auto' di sini -->
+
+<div class="relative top-[125px] w-[900px]  overflow-x-auto shadow-md sm:rounded-lg table-auto"> <!-- Tambahkan class 'mx-auto' di sini -->
     <table class="relative w-full overflow-x-auto shadow-md sm:rounded-lg ">
         <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
             <tr>
-                <th scope="col" class="px-6 py-3 text-left">
-                    Product Name
+                <th scope="col" class="px-6 py-3 text-left w-12">
+                    #
                 </th>
-                <th scope="col" class="px-6 py-3 text-left">
-                    Color
+                <th scope="col" class="px-6 py-3 text-left w-48">
+                    Name
                 </th>
-                <th scope="col" class="px-6 py-3 text-left">
-                    Category
+                <th scope="col" class="px-6 py-3 text-left w-32">
+                    Username
                 </th>
-                <th scope="col" class="px-6 py-3 text-left">
-                    Price
+                <th scope="col" class="px-6 py-3 text-left w-32">
+                    Email
                 </th>
-                <th scope="col" class="px-6 py-3 text-left">
+                <th scope="col" class="px-6 py-3 text-left w-32">
                     Action
                 </th>
+                
             </tr>
         </thead>
+        
         <tbody>
             <tr class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
                 <td class="px-6 py-4 whitespace-nowrap font-medium text-gray-900 dark:text-white">
-                    Apple MacBook Pro 17"
+                    1
                 </td>
                 <td class="px-6 py-4">
-                    Silver
+                    Haykal
                 </td>
                 <td class="px-6 py-4">
-                    Laptop
+                    HLhere
                 </td>
                 <td class="px-6 py-4">
-                    $2999
+                    Laptop@gmail.com
+                </td>   
+                <td class="px-6 py-4 flex items-center ">
+                    <a href="#" class="font-medium text-yellow-400 dark:text-blue-500 hover:underline w-8">Edit</a>
+                    <span class="mx-1">|</span>
+                    <a href="#" class="font-medium text-red-500 dark:text-red-500 hover:underline w-8">Delete</a>
                 </td>
-                <td class="px-6 py-4 flex items-center">
-                    <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
-                    <span class="mx-2">|</span>
-                    <a href="#" class="font-medium text-red-500 dark:text-red-500 hover:underline">Hapus</a>
-                </td>
+                
+                
             </tr>
             <!-- Add more rows as needed -->
         </tbody>
     </table>
 </div>
-
 @endsection
