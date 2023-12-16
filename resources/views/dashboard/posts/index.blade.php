8c6991a56be4be5fd4e12750e0aa48e98817f2ec
HL <kalfearislit@gmail.com>
2023-12-16 18:50:07 +0700

    menamabahkan table logic

--- resources/views/dashboard/posts/index.blade.php
+++ resources/views/dashboard/posts/index.blade.php
@@ -5,7 +5,7 @@
 
 @section('container')
 
-
+{{-- {{dd($posts)}} --}}
 
 <div class="relative top-[125px] w-[900px]  overflow-x-auto shadow-md sm:rounded-lg table-auto"> <!-- Tambahkan class 'mx-auto' di sini -->
     <table class="relative w-full overflow-x-auto shadow-md sm:rounded-lg ">
@@ -26,28 +26,29 @@
                 
             </tr>
         </thead>
+
+        @foreach ($posts as $post)
         
         <tbody>
             <tr class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
                 <td class="px-6 py-4 whitespace-nowrap font-medium text-gray-900 dark:text-white">
-                    1
+                    {{$post->id}}
                 </td>
                 <td class="px-6 py-4">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, alias ipsa similique iure pariatur qui veritatis accusamus commodi officia eligendi vel. Delectus cum corrupti laudantium, ipsum ullam nisi. Dicta, nostrum.
+                    {{$post->title}}
                 </td>
                 <td class="px-6 py-4">
-                    Laptop
+                   {{$post->category}}
                 </td>   
                 <td class="px-6 py-4 flex items-center ">
                     <a href="#" class="font-medium text-yellow-400 dark:text-blue-500 hover:underline w-8">Edit</a>
                     <span class="mx-1">|</span>
                     <a href="#" class="font-medium text-red-500 dark:text-red-500 hover:underline w-8">Delete</a>
                 </td>
-                
-                
             </tr>
             <!-- Add more rows as needed -->
         </tbody>
+        @endforeach
     </table>
 </div>
 
