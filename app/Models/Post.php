087450a4b8a4f477f301b9ff1116345239d4c8e0
M-Ridho-Fauzan <ridho.213040036@mail.unpas.ac.id>
2023-12-23 03:28:45 +0700

    menambahkan fungsi search bukan live setengah jadi, jika ingin melanjutkan silahkan :)

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -42,4 +42,30 @@ class Post extends Model
             ]
         ];
     }
+
+    public function scopeFilter($query, array $filters)
+    {
+        $query->when($filters['search'] ?? false, function ($query, $search) {
+            return $query->where(function ($query) use ($search) {
+                $query->where('title', 'like', '%' . $search . '%')
+                    ->orWhere('body', 'like', '%' . $search . '%');
+            });
+        });
+
+        $query->when($filters['category'] ?? false, function ($query, $category) {
+            return $query->whereHas('category', function ($query) use ($category) {
+                $query->where('slug', $category); // mencari dengan menyamakan slug == $category
+            });
+        });
+
+        $query->when(
+            $filters['author'] ?? false,
+            fn ($query, $author) =>
+            $query->whereHas(
+                'author',
+                fn ($query) =>
+                $query->where('username', $author) // mencari dengan menyamakan username == $author
+            )
+        );
+    }
 }
