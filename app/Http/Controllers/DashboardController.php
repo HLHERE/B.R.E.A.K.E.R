b555d3d936d803275b827c7ae3f196d8e84877d2
syahidanfitrah <syahidanfitrah9@gmail.com>
2023-12-27 16:05:17 +0700

    Membuat gate untuk admin

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -9,6 +9,7 @@ use Illuminate\Http\Request;
 class DashboardController extends Controller
 {
     
+    
 
     public function index() {
 
--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -22,7 +22,7 @@ class LoginController extends Controller
         if(Auth::attempt($credentials)) {
             $request->session()->regenerate();
 
-            return redirect()->intended('/dashboard');
+            return redirect()->intended('/');
         }
 
         return back()->with('loginError', 'Login failed!');
