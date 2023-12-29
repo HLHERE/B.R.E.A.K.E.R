15de85000294726b3f39e92c713fde0d665c35e1
M-Ridho-Fauzan <ridho.213040036@mail.unpas.ac.id>
2023-12-29 13:27:04 +0700

    Menambahkan category di home bag 2 *NOTE: Foto di category itu blom bener, harus di sesuaikan sama ukuran nya

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -70,8 +70,10 @@
                         <h1 class="text-7xl font-Ghetto title-font mb-2 text-abu">Hot News</h1>
                         <div class="h-1 w-20 bg-coklat rounded"></div>
                     </div>
-                    <p class="lg:w-1/2 w-full leading-relaxed text-gray-500">"Dive into the pulse of the latest headlines with Glam Groove News! From captivating global events to exclusive narratives, 
-                        Glam Groove brings you the hottest stories in a unique blend of sophistication. Experience news like never before — where every headline is a celebration of the extraordinary. 
+                    <p class="lg:w-1/2 w-full leading-relaxed text-gray-500">"Dive into the pulse of the latest headlines
+                        with Glam Groove News! From captivating global events to exclusive narratives,
+                        Glam Groove brings you the hottest stories in a unique blend of sophistication. Experience news like
+                        never before — where every headline is a celebration of the extraordinary.
                         Stay tuned for the epitome of sophistication and style, exclusively at Glam Groove."</p>
                 </div>
         </section>
@@ -313,14 +315,14 @@
                                     <div class="sm:w-1/2 relative">
                                         <div>
                                             <p class="p-6 text-xs font-medium leading-3 text-white absolute top-0 right-0">
-                                                12
-                                                Agustus 2002</p>
+                                                {{ $categoryList[0]->created_at->diffForHumans() }}</p>
                                             <div class="absolute bottom-0 left-0 p-6">
-                                                <h2 class="text-xl font-semibold 5 text-white">Artbook</h2>
+                                                <h2 class="text-xl font-semibold 5 text-white">
+                                                    {{ $categoryList[0]->name }}</h2>
                                                 <p class="text-base leading-4 text-white mt-2">Rasakan kembali kekayaan
                                                     alam
                                                     Itomori</p>
-                                                <a href="javascript:void(0)"
+                                                <a href="/posts?category={{ $categoryList[0]->slug }}"
                                                     class="focus:outline-none focus:underline flex items-center mt-4 cursor-pointer text-white hover:text-gray-200 hover:underline">
                                                     <p class="pr-2 text-sm font-medium leading-none">Read More</p>
                                                     <svg class="fill-stroke" width="16" height="16"
@@ -333,19 +335,20 @@
                                                 </a>
                                             </div>
                                         </div>
-                                        <img src="/../img/Hujan.jpg" alt="hujan" class="w-full" alt="chair" />
+                                        <img src="https://source.unsplash.com/1200x400?{{ $categoryList[0]->name }}"
+                                            alt="hujan" class="w-full" alt="chair" />
                                     </div>
                                     <div class="sm:w-1/2 sm:mt-0 mt-4 relative">
                                         <div>
                                             <p class="p-6 text-xs font-medium leading-3 text-white absolute top-0 right-0">
-                                                12 April
-                                                2021</p>
+                                                {{ $categoryList[1]->created_at->diffForHumans() }}</p>
                                             <div class="absolute bottom-0 left-0 p-6">
-                                                <h2 class="text-xl font-semibold 5 text-white">Suzume No Tojimari</h2>
+                                                <h2 class="text-xl font-semibold 5 text-white">
+                                                    {{ $categoryList[1]->name }}</h2>
                                                 <p class="text-base leading-4 text-white mt-2">Perjalanan Suzume dimulai
                                                     dari
                                                     sebuah kota di Kyushu</p>
-                                                <a href="javascript:void(0)"
+                                                <a href="/posts?category={{ $categoryList[1]->slug }}"
                                                     class="focus:outline-none focus:underline flex items-center mt-4 cursor-pointer text-white hover:text-gray-200 hover:underline">
                                                     <p class="pr-2 text-sm font-medium leading-none">Read More</p>
                                                     <svg class="fill-stroke" width="16" height="16"
@@ -358,20 +361,21 @@
                                                 </a>
                                             </div>
                                         </div>
-                                        <img src="/../img/Gempa.jpg" class="w-full" alt="gempa" class="w-full" />
+                                        <img src="https://source.unsplash.com/1200x400?{{ $categoryList[1]->name }}"
+                                            class="w-full" alt="gempa" class="w-full" />
                                     </div>
                                 </div>
                                 <div class="relative">
                                     <div>
                                         <p
                                             class="md:p-10 p-6 text-xs font-medium leading-3 text-white absolute top-0 right-0">
-                                            12
-                                            April 2021</p>
+                                            {{ $categoryList[2]->created_at->diffForHumans() }}</p>
                                         <div class="absolute bottom-0 left-0 md:p-10 p-6">
-                                            <h2 class="text-xl font-semibold 5 text-white">Valorant</h2>
+                                            <h2 class="text-xl font-semibold 5 text-white">{{ $categoryList[2]->name }}
+                                            </h2>
                                             <p class="text-base leading-4 text-white mt-2">7 Agent Terbaik Versi Team
                                                 Secret</p>
-                                            <a href="javascript:void(0)"
+                                            <a href="/posts?category={{ $categoryList[2]->slug }}"
                                                 class="focus:outline-none focus:underline flex items-center mt-4 cursor-pointer text-white hover:text-gray-200 hover:underline">
                                                 <p class="pr-2 text-sm font-medium leading-none">Read More</p>
                                                 <svg class="fill-stroke" width="16" height="16"
@@ -384,8 +388,8 @@
                                             </a>
                                         </div>
                                     </div>
-                                    <img src="/../img/valor.jpg" alt="sitting place"
-                                        class="w-full md:mt-9 hidden sm:block" />
+                                    <img src="https://source.unsplash.com/1200x400?{{ $categoryList[2]->name }}"
+                                        alt="sitting place" class="w-full md:mt-9 hidden sm:block" />
                                 </div>
                             </div>
                             <div class="lg:w-1/2 xl:ml-8 lg:ml-4 lg:mt-0 md:mt-6 mt-4 lg:flex flex-col justify-between">
@@ -393,14 +397,14 @@
                                     <div>
                                         <p
                                             class="md:p-10 p-6 text-xs font-medium leading-3 text-white absolute top-0 right-0">
-                                            12
-                                            April 2021</p>
+                                            {{ $categoryList[3]->created_at->diffForHumans() }}</p>
                                         <div class="absolute bottom-0 left-0 md:p-10 p-6">
-                                            <h2 class="text-xl font-semibold 5 text-white">Artificial Intelligence</h2>
+                                            <h2 class="text-xl font-semibold 5 text-white">{{ $categoryList[3]->name }}
+                                            </h2>
                                             <p class="text-base leading-4 text-white mt-2">Perkembangan AI dapat berdampak
                                                 negatif
                                             </p>
-                                            <a href="javascript:void(0)"
+                                            <a href="/posts?category={{ $categoryList[3]->slug }}"
                                                 class="focus:outline-none focus:underline flex items-center mt-4 cursor-pointer text-white hover:text-gray-200 hover:underline">
                                                 <p class="pr-2 text-sm font-medium leading-none">Read More</p>
                                                 <svg class="fill-stroke" width="16" height="16"
@@ -413,7 +417,8 @@
                                             </a>
                                         </div>
                                     </div>
-                                    <img src="/../img/AI.jpg" alt="sitting place" class="w-full sm:block hidden" />
+                                    <img src="https://source.unsplash.com/1200x400?{{ $categoryList[3]->name }}"
+                                        alt="sitting place" class="w-full sm:block hidden" />
                                     <img class="w-full sm:hidden" src="https://i.ibb.co/dpXStJk/Rectangle-29.png"
                                         alt="sitting place" />
                                 </div>
@@ -421,14 +426,14 @@
                                     <div class="relative w-full">
                                         <div>
                                             <p class="p-6 text-xs font-medium leading-3 text-white absolute top-0 right-0">
-                                                12 April
-                                                2021</p>
+                                                {{ $categoryList[4]->created_at->diffForHumans() }}</p>
                                             <div class="absolute bottom-0 left-0 p-6">
-                                                <h2 class="text-xl font-semibold 5 text-white">Jujutsu Kaisen</h2>
+                                                <h2 class="text-xl font-semibold 5 text-white">
+                                                    {{ $categoryList[4]->name }}</h2>
                                                 <p class="text-base leading-4 text-white mt-2">Pertarungan Gojo Satoru &
                                                     Toji
                                                     Fushiguro</p>
-                                                <a href="javascript:void(0)"
+                                                <a href="/posts?category={{ $categoryList[4]->slug }}"
                                                     class="focus:outline-none focus:underline flex items-center mt-4 cursor-pointer text-white hover:text-gray-200 hover:underline">
                                                     <p class="pr-2 text-sm font-medium leading-none">Read More</p>
                                                     <svg class="fill-stroke" width="16" height="16"
@@ -441,19 +446,20 @@
                                                 </a>
                                             </div>
                                         </div>
-                                        <img src="/../img/jujutsu.jpg" class="w-full" alt="chair" />
+                                        <img src="https://source.unsplash.com/1200x400?{{ $categoryList[4]->name }}"
+                                            class="w-full" alt="chair" />
                                     </div>
                                     <div class="relative w-full sm:mt-0 mt-4">
                                         <div>
                                             <p class="p-6 text-xs font-medium leading-3 text-white absolute top-0 right-0">
-                                                12 April
-                                                2021</p>
+                                                {{ $categoryList[5]->created_at->diffForHumans() }}</p>
                                             <div class="absolute bottom-0 left-0 p-6">
-                                                <h2 class="text-xl font-semibold 5 text-white">Anthem</h2>
+                                                <h2 class="text-xl font-semibold 5 text-white">
+                                                    {{ $categoryList[5]->name }}</h2>
                                                 <p class="text-base leading-4 text-white mt-2">Anthem mengambil setting di
                                                     sebuah
                                                     dunia misterius</p>
-                                                <a href="javascript:void(0)"
+                                                <a href="/posts?category={{ $categoryList[5]->slug }}"
                                                     class="focus:outline-none focus:underline flex items-center mt-4 cursor-pointer text-white hover:text-gray-200 hover:underline">
                                                     <p class="pr-2 text-sm font-medium leading-none">Read More</p>
                                                     <svg class="fill-stroke" width="16" height="16"
@@ -466,7 +472,8 @@
                                                 </a>
                                             </div>
                                         </div>
-                                        <img src="/../img/anthem.jpg" class="w-full" alt="wall design" />
+                                        <img src="https://source.unsplash.com/1200x400?{{ $categoryList[5]->name }}"
+                                            class="w-full" alt="wall design" />
                                     </div>
                                 </div>
                             </div>
