b69c7773db2f63fd6f357b362641a5b6a5611cce
HL <kalfearislit@gmail.com>
2023-12-30 17:15:00 +0700

    hapus search

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -11,32 +11,6 @@
                             {{ $judul }}</h1>
                         <div class="h-1 w-20 bg-coklat_muda rounded"></div>
 
-                        <!-- Tombol Search -->
-                        <div class="flex mt-5">
-                            <form action="/posts">
-                                @if (request('category'))
-                                    <input type="hidden" name="category" value="{{ request('category') }}">
-                                @endif
-                                @if (request('author'))
-                                    <input type="hidden" name="author" value="{{ request('author') }}">
-                                @endif
-                                <div class="relative text-gray-600 focus-within:text-gray-400">
-                                    <span class="absolute inset-y-0 left-0 flex items-center pl-2">
-                                        <button type="submit" class="p-1 focus:outline-none focus:shadow-outline">
-                                            <svg fill="none" stroke="currentColor" stroke-linecap="round"
-                                                stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24"
-                                                class="w-6 h-6">
-                                                <path d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
-                                            </svg>
-                                        </button>
-                                    </span>
-                                    <input type="search" name="search" value="{{ request('search') }}"
-                                        class="py-3 text-sm pr-52 text-white rounded-md pl-16 focus:outline-none focus:bg-white focus:text-gray-900"
-                                        placeholder="Search..." autocomplete="off">
-                                </div>
-                            </form>
-                        </div>
-                        <!-- End Tombol Search -->
                     </div>
                 </div>
 
