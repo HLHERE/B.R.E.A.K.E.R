81c6943b885ae7b8189e6c9bae8bbe24df478e88
M-Ridho-Fauzan <ridho.213040036@mail.unpas.ac.id>
2023-12-30 15:31:54 +0700

    menambahkan search no live

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -3,16 +3,6 @@
 @section('container')
     <!-- component -->
     <div class="max-w-screen-lg mx-auto">
-        {{-- Hidden Request, query category by search --}}
-        <form action="/posts">
-            @if (request('category'))
-                <input type="hidden" name="category" value="{{ request('category') }}">
-            @endif
-            @if (request('author'))
-                <input type="hidden" name="author" value="{{ request('author') }}">
-            @endif
-        </form>
-
         <section class="text-gray-600 body-font">
             <div class="container px-5 py-24 mx-auto mt-1">
                 <div class="flex flex-wrap w-full mb-20">
@@ -20,20 +10,32 @@
                         <h1 class="sm:text-5xl text-2xl font-normal font-Ghetto title-font mb-2 text-abu">
                             {{ $judul }}</h1>
                         <div class="h-1 w-20 bg-coklat_muda rounded"></div>
-    <!-- Tombol Search -->
-    <div class="flex mt-5">
-        <form method="GET">
-            <div class="relative text-gray-600 focus-within:text-gray-400">
-            <span class="absolute inset-y-0 left-0 flex items-center pl-2">
-                <button type="submit" class="p-1 focus:outline-none focus:shadow-outline">
-                    <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-6 h-6"><path d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
-                </button>
-            </span>
-                <input type="search" name="q" class="py-3 text-sm pr-52 text-white rounded-md pl-16 focus:outline-none focus:bg-white focus:text-gray-900" placeholder="Search..." autocomplete="off">
-            </div>
-        </form>
-    </div>
-    <!-- End Tombol Search -->
+                        <!-- Tombol Search -->
+                        <div class="flex mt-5">
+                            <form action="/posts">
+                                @if (request('category'))
+                                    <input type="hidden" name="category" value="{{ request('category') }}">
+                                @endif
+                                @if (request('author'))
+                                    <input type="hidden" name="author" value="{{ request('author') }}">
+                                @endif
+                                <div class="relative text-gray-600 focus-within:text-gray-400">
+                                    <span class="absolute inset-y-0 left-0 flex items-center pl-2">
+                                        <button type="submit" class="p-1 focus:outline-none focus:shadow-outline">
+                                            <svg fill="none" stroke="currentColor" stroke-linecap="round"
+                                                stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24"
+                                                class="w-6 h-6">
+                                                <path d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
+                                            </svg>
+                                        </button>
+                                    </span>
+                                    <input type="search" name="search" value="{{ request('search') }}"
+                                        class="py-3 text-sm pr-52 text-white rounded-md pl-16 focus:outline-none focus:bg-white focus:text-gray-900"
+                                        placeholder="Search..." autocomplete="off">
+                                </div>
+                            </form>
+                        </div>
+                        <!-- End Tombol Search -->
                     </div>
                 </div>
 
