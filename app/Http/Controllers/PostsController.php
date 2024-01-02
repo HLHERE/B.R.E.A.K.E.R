64f07c3012b3fdc809b425152aac02186cfec9b1
M-Ridho-Fauzan <ridho.213040036@mail.unpas.ac.id>
2024-01-02 14:14:19 +0700

    pembenaran error saat di web host

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -1,9 +1,61 @@
 <?php
 
+// namespace App\Http\Controllers;
+
+// use App\Models\Post;
+// use Illuminate\Http\Request;
+// use App\Services\PostsService;
+// use App\Traits\CategoryListTrait;
+
+// class PostsController extends Controller
+// {
+//     use CategoryListTrait;
+//     protected $postsService;
+
+//     public function __construct(PostsService $postService)
+//     {
+//         $this->postsService = $postService;
+//     }
+
+//     public function liveSearch(Request $request)
+//     {
+//         $query = $request->input('query');
+
+//         $searchResult = Post::latest()
+//             ->filter(['search' => $query])
+//             ->get();
+
+//         $results = $searchResult->map(function ($post) {
+//             return [
+//                 'title' => $post->title,
+//                 'url' => route('posts.show', $post), // Sesuaikan ini dengan nama route yang sesuai
+//             ];
+//         });
+
+//         return response()->json($results);
+//     }
+
+//     public function index()
+//     {
+//         $this->shareCategoryList();
+
+//         $getData = $this->postsService->postsService();
+//         $liveSearch = $this->liveSearch(request());
+
+//         return view('posts', [
+//             "judul" => $getData->title,
+//             "posts" => $getData->posts
+//         ]);
+//     }
+// }
+
 namespace App\Http\Controllers;
 
-use App\Services\PostsService;
+use App\Models\Post;
 use App\Traits\CategoryListTrait;
+
+use Illuminate\Http\Request;
+use App\Services\PostsService;
 
 class PostsController extends Controller
 {
@@ -15,15 +67,35 @@ class PostsController extends Controller
         $this->postsService = $postService;
     }
 
-    public function index()
+    public function liveSearch(Request $request)
+    {
+        $query = $request->input('query');
+
+        $searchResult = Post::latest()
+            ->filter(['search' => $query])
+            ->get();
+
+        $results = $searchResult->map(function ($post) {
+            return [
+                'title' => $post->title,
+                'url' => route('posts.show', $post), // Sesuaikan ini dengan nama route yang sesuai
+            ];
+        });
+        // dd(response()->json($results));
+        return response()->json($results); // Pastikan respons dikirim dalam format JSON
+    }
+
+    public function index(Request $request)
     {
         $this->shareCategoryList();
 
         $getData = $this->postsService->postsService();
+        $liveSearchResults = $this->liveSearch($request);
 
         return view('posts', [
             "judul" => $getData->title,
-            "posts" => $getData->posts
+            "posts" => $getData->posts,
+            "liveSearchResults" => $liveSearchResults->getData(), // Ambil data dari response JSON
         ]);
     }
 }
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -1,17 +1,20 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>GlamGroove Entertainment</title>
-    @vite(['resources/css/app.css','resources/js/app.js'])
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
-<body >
+
+<body>
     @include('partials.navbar')
 
     <div class="container bg-putih">
-          @yield('container')
+        @yield('container')
     </div>
     @include('partials.footer')
 </body>
+
 </html>
